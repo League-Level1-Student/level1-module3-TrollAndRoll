2c00dd07912530b68659843be499ba6e1a04ca80
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-23 13:29:56 -0700

    Whack A Mole IP

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,39 +1,109 @@
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole 
+import org.jointheleague.graphical.robot.Robot;
+
+public class WhackAMole implements ActionListener 
 {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	Random randy = new Random();
 	
-	JButton b1 = new JButton();
-	JButton b2 = new JButton();
-	JButton b3 = new JButton();
-	JButton b4 = new JButton();
-	JButton b5 = new JButton();
-	JButton b6 = new JButton();
-	JButton b7 = new JButton();
-	JButton b8 = new JButton();
-	JButton b9 = new JButton();
-	
+	int moles = 0;
+	boolean found = true;
+
 	public static void main(String[] args) 
 	{
 		new WhackAMole().run();
 	}
-	
-	public void run()
+
+	public void run() 
 	{
 		frame.setVisible(true);
 		frame.setTitle("Whack a button for Fame and Glory");
-		frame.setSize(300, 500);
+		frame.setSize(300, 300);
+		
+		while(moles < 10)
+		{
+			if(found = true)
+			{
+				drawButtons(randy.nextInt(9));
+				found = false;
+			}
+			
+		}
+		
+		//endGame(0, 10);
 	}
-	
-	void drawButtons(Random randy)
+
+	void drawButtons(int hi) 
 	{
+		for (int i = 0; i < 9; i++) 
+		{
+			if(i == hi)
+			{
+				JButton butter = new JButton("mole!");
+				butter.setPreferredSize(new Dimension(100, 40));
+				butter.addActionListener(this);
+				panel.add(butter);
+			}
+			
+			else
+			{
+				JButton butt = new JButton();
+				butt.setPreferredSize(new Dimension(100, 40));
+				butt.addActionListener(this);
+				panel.add(butt);
+			}
+		}
+		frame.add(panel);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		// TODO Auto-generated method stub
+		JButton pressed = (JButton) arg0.getSource();
+		if(pressed.getText().equals("mole!"))
+		{
+			speak("You Missed!");
+		}
+		else
+		{
+			moles = moles + 1;
+			frame.dispose();
+			found = true;
+		}
 		
 	}
+	
+	void speak(String words) 
+	{
+	     try 
+	     {
+	          Runtime.getRuntime().exec("say " + words).waitFor();
+	     } catch (Exception e) 
+	     {
+	          e.printStackTrace();
+	     }
+	}
+	
+	/*
+	private void endGame(Date timeAtStart, int molesWhacked) 
+	{
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	          + " moles per second.");
+	}
+	*/
 }
