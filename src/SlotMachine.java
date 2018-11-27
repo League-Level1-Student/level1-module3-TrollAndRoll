429a259195cebde5bbd62766e14c18f6d9401fe7
329525 <329525@0338S-S-R0B6ZHH.sdcs.local>
2018-11-27 14:29:12 -0800

    Slot Machine IP

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,4 +1,7 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -7,7 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SlotMachine 
+public class SlotMachine implements ActionListener
 {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
@@ -18,11 +21,11 @@ public class SlotMachine
 	
 	JButton spin = new JButton();
 	
-	String orange = "orange.jpg";
+	String orange = "orange.png";
 	String cherry = "cherry.png";
 	String lime = "lime.png";
 	
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args)  
 	{
 		SlotMachine runner = new SlotMachine();	
 		runner.run();
@@ -30,6 +33,8 @@ public class SlotMachine
 	
 	private void run()
 	{
+		spin.addActionListener(this);
+		
 		label1.setIcon(loadImage(orange));
 		label2.setIcon(loadImage(cherry));
 		label3.setIcon(loadImage(lime));
@@ -55,6 +60,56 @@ public class SlotMachine
 		{
 			
 			return null;
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		// TODO Auto-generated method stub
+		int reel1 = new Random().nextInt(3)+1;
+		int reel2 = new Random().nextInt(3)+1;
+		int reel3 = new Random().nextInt(3)+1;
+		
+		if(reel1 == 1)
+		{
+			label1.setIcon(loadImage(orange));
+		}
+		else if(reel1 == 2)
+		{
+			label1.setIcon(loadImage(cherry));
+		}
+		else if (reel1 == 3)
+		{
+			label1.setIcon(loadImage(lime));
+		}
+		if(reel2 == 1)
+		{
+			label1.setIcon(loadImage(orange));
+		}
+		else if(reel2 == 2)
+		{
+			label1.setIcon(loadImage(cherry));
+		}
+		else if (reel2 == 3)
+		{
+			label1.setIcon(loadImage(lime));
+		}
+		if(reel3 == 1)
+		{
+			label1.setIcon(loadImage(orange));
+		}
+		else if(reel3 == 2)
+		{
+			label1.setIcon(loadImage(cherry));
+		}
+		else if (reel3 == 3)
+		{
+			label1.setIcon(loadImage(lime));
+		}
+		if(reel1 == reel2 && reel1 == reel3)
+		{
+			
 		}
 	}
 }
